81b3dd08f3912009d3a5b5315ace24150bbd452b
yvettetuyizere <yvettetuyizere@gmail.com>
2026-04-25 17:07:03 +0200

    Revert "Restore all pages and components from main branch"
    This reverts commit 4e09ede9f259a43bb46d3606b1f206ff2db2b6c3.

--- rrh-frontend/src/components/shared/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all shared components
-export { Navbar } from "./Navbar";
-export { Footer } from "./Footer";
-export { AuthSide } from "./AuthSide";
-export { DashMap } from "./DashMap";
-export { BarChart, LineChart, RainfallChart, RiverLevelChart, MLAccuracyChart } from "./Charts";
