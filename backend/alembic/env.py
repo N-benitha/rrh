e81608f4d6cf3a8be530065da3c3cf74f83ff5ad
N-benitha <ngungabn03@gmail.com>
2026-06-17 08:39:10 +0200

    feat: asserting python version for render

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -12,10 +12,12 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "backend"))
 
 from app.database import Base
 from app.models import Users, Region, SensorReading, Prediction, Alert, UserRegionSubscription  # noqa: F401
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
